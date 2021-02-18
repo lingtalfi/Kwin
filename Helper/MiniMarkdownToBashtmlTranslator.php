48e10f6618418440cef89e5296f37d39017d5d98
lingtalfi <lingtalfi@gmail.com>
2021-02-18 08:40:26 +0100

    update initial commit

---
+++ Helper/MiniMarkdownToBashtmlTranslator.php
@@ -0,0 +1,90 @@
+<?php
+
+
+namespace Ling\Kwin\Helper;
+
+/**
+ * The MiniMarkdownToBashtmlTranslator class.
+ */
+class MiniMarkdownToBashtmlTranslator
+{
+
+
+    /**
+     * Converts a [mini-markdown](https://github.com/lingtalfi/TheBar/blob/master/discussions/kwin-notation.md#mini-markdown) string to its @page(bashtml) equivalent.
+     *
+     * Available options are:
+     * - fmtText: string=green, the bashtml format to use for the text part of a link
+     * - fmtUrl: string=blue, the bashtml format to use for the url part of a link
+     *
+     *
+     *
+     * @param string $string
+     * @param array $options
+     * @return string
+     */
+    public static function convertString(string $string, array $options = []): string
+    {
+        if (true === is_numeric($string)) {
+            return $string;
+        }
+
+        $co = $options["fmtText"] ?? 'green';
+        $ur = $options["fmtUrl"] ?? 'blue';
+
+        $string = preg_replace_callback('!\*\*([^*]+)\*\*!m', function (array $match) {
+            $key = $match[1];
+            return "<b>$key</b>";
+        }, $string);
+
+
+        $string = preg_replace_callback('!\[([^\]]+)\]\(([^)]+)\)!', function (array $match) use ($co, $ur) {
+            $text = $match[1];
+            $url = $match[2];
+            return "<$co>$text</$co>(<$ur>$url</$ur>)";
+        }, $string);
+        return $string;
+    }
+
+
+    /**
+     * Converts the [mini-markdown](https://github.com/lingtalfi/TheBar/blob/master/discussions/kwin-notation.md#mini-markdown)) to @page(bashtml) in the given array, and returns the result.
+     * Both the keys and the values are translated.
+     *
+     * @param array $arr
+     * @return array
+     */
+    public static function convertArray(array $arr): array
+    {
+        $ret = [];
+        self::convertArrayRecursive($arr, $ret);
+        return $ret;
+    }
+
+
+    //--------------------------------------------
+    //
+    //--------------------------------------------
+    /**
+     * Converts the mini-markdown to bashtml in the given array, and place it in the given $ret variable.
+     *
+     * @param array $arr
+     * @param array $ret
+     */
+    private static function convertArrayRecursive(array $arr, array &$ret)
+    {
+        foreach ($arr as $key => $value) {
+            $key = self::convertString($key);
+            if (true === is_array($value)) {
+                $tmpArr = [];
+                self::convertArrayRecursive($value, $tmpArr);
+                $ret[$key] = $tmpArr;
+            } else {
+                $value = self::convertString($value);
+                $ret[$key] = $value;
+            }
+        }
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+Kwin
+===========
+2021-02-18
+
+
+
+Some tools to work with the [kwin](https://github.com/lingtalfi/TheBar/blob/master/discussions/kwin-notation.md) syntax.
+
+
+This is part of the [universe framework](https://github.com/karayabin/universe-snapshot).
+
+
+Install
+==========
+Using the [uni](https://github.com/lingtalfi/universe-naive-importer) command.
+```bash
+uni import Ling/Kwin
+```
+
+Or just download it and place it where you want otherwise.
+
+
+
+
+
+
+Summary
+===========
+- [Kwin api](https://github.com/lingtalfi/Kwin/blob/master/doc/api/Ling/Kwin.md) (generated with [DocTools](https://github.com/lingtalfi/DocTools))
+
+
+
+
+
+
+History Log
+=============
+
+- 1.0.0 -- 2021-02-18
+
+    - initial commit
